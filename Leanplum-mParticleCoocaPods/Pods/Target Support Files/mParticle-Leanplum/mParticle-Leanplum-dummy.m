b4d804383faf4cdde032bfc9e1210a47d47e2bc7
Sophie Saouma <sophie.saouma@gmail.com>
2016-10-03 13:41:28 -0700

    Leanplum-mParticle Integration
    Sample project of the mParticle Integration

---
+++ Leanplum-mParticleCoocaPods/Pods/Target Support Files/mParticle-Leanplum/mParticle-Leanplum-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_mParticle_Leanplum : NSObject
+@end
+@implementation PodsDummy_mParticle_Leanplum
+@end
---
+++ Leanplum-mParticleCoocaPods/ViewControllerSecond.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerSecond.h
+//  
+//
+//  Created by Sophie Saouma on 9/26/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerSecond : UIViewController
+
+@end
