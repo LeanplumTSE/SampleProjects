460bab479c3e29fa6d0948e323015253673107f3
federico1525 <f.casali@gmail.com>
2016-10-07 16:10:49 -0700

    mParticle sampel update

--- Android_mParticle/app/src/main/java/com/leanplum/android_mparticle/ApplicationClass.java
+++ Android_mParticle/app/src/main/java/com/leanplum/android_mparticle/ApplicationClass.java
@@ -8,10 +8,13 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.leanplum.Leanplum;
+import com.leanplum.LeanplumActivityHelper;
 import com.leanplum.LeanplumPushNotificationCustomizer;
 import com.leanplum.LeanplumPushService;
 import com.leanplum.annotations.Parser;
+import com.leanplum.annotations.Variable;
 import com.leanplum.callbacks.StartCallback;
+import com.leanplum.callbacks.VariablesChangedCallback;
 import com.mparticle.MParticle;
 
 /**
@@ -20,9 +23,24 @@ import com.mparticle.MParticle;
 
 public class ApplicationClass extends Application {
 
+    public enum Environment {
+        AutoDetect,
+        Development,
+        Production;
+    }
+
+    @Variable
+    public static String welcomeString = "ddd";
+
+
+    @Variable
+    public static int StartupScreen = 0;
+
+
     @Override
     public void onCreate() {
         super.onCreate();
+
 
         LeanplumPushService.setGcmSenderId(LeanplumPushService.LEANPLUM_SENDER_ID);
 
@@ -48,10 +66,39 @@ public class ApplicationClass extends Application {
         // Parsing for Variable to be registered in Leanplum Dashboard
         // This has to be done BEFORE starting Leanplum
         // Variables are defined in this case into LPvariables class
-        Parser.parseVariablesForClasses(LPvariables.class);
+        Parser.parseVariables(this);
+        Parser.parseVariablesForClasses(LPvariables.class, MainActivity.class);
+
+
+        // Place here DeviceID code :
 
         // Starting MParticle - this will also start Leanplum
-        MParticle.start(this);
+//        MParticle.start(this, MParticle.InstallType.AutoDetect, MParticle.Environment.Production);
+
+
+        Leanplum.addVariablesChangedHandler(new VariablesChangedCallback() {
+            @Override
+            public void variablesChanged() {
+                if (StartupScreen == 0){
+                    Log.i("### ", "variable retrieved");
+                }
+
+                else {
+                    // show something
+                }
+            }
+        });
+
+        Leanplum.addStartResponseHandler(new StartCallback() {
+            @Override
+            public void onResponse(boolean b) {
+                Log.i("### ", "Leanplum started -- application class");
+            }
+        });
+
+
+        MParticle.start(this, MParticle.InstallType.AutoDetect, MParticle.Environment.Production);
+//        MParticle.start(this);
 
 
     }
--- Android_mParticle/app/src/main/java/com/leanplum/android_mparticle/MainActivity.java
+++ Android_mParticle/app/src/main/java/com/leanplum/android_mparticle/MainActivity.java
@@ -7,19 +7,41 @@ import android.view.View;
 import android.widget.EditText;
 
 import com.leanplum.Leanplum;
+import com.leanplum.annotations.Variable;
 import com.leanplum.callbacks.StartCallback;
 import com.leanplum.callbacks.VariablesChangedCallback;
 import com.mparticle.MPEvent;
 import com.mparticle.MParticle;
+import com.mparticle.commerce.CommerceEvent;
+import com.mparticle.commerce.Product;
+import com.mparticle.commerce.TransactionAttributes;
 import com.mparticle.kits.LeanplumKit;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     public static EditText mEdit;
     public static String userID;
+
+    // Ignore the following
+//    public static Map<String, Object> LeanplumAttributes = new HashMap<String, Object>();
+//    public static List<Object> products = new ArrayList<Object>();
+
+
+    // creating a product for the Purchase event
+    Product product = new Product.Builder("Foo name", "Foo sku", 100.00)
+            .quantity(4)
+            .build();
+
+    // creating a transaction ID required for PURCHASE and REFUND events
+    TransactionAttributes attributes = new TransactionAttributes("foo-transaction-id")
+            .setRevenue(450.00)
+            .setTax(30.00)
+            .setShipping(20.00);
 
 
     public void setUserID(View view){
@@ -30,7 +52,57 @@ public class MainActivity extends AppCompatActivity {
         userID = null;
     }
 
+    public void setUserAttributes(View view){
+
+        // setting UserAttributes
+        MParticle.getInstance().setUserAttribute("name", "federico");
+        MParticle.getInstance().setUserAttribute("email", "fede@gmail.com");
+
+
+        //associate a list of values with an attribute key
+        List<String> attributeList = new ArrayList<>();
+        attributeList.add("Apple");
+        attributeList.add("Orange");
+        attributeList.add("Peach");
+        MParticle.getInstance().setUserAttributeList("Favorite Fruits", attributeList);
+
+
+        // Ignore the following test
+        // Adding Leanplum UserAttributes as an Array
+//        products.add("paperino");
+//        products.add("pluto");
+//
+//        LeanplumAttributes.put("otherNames", products);
+//        Leanplum.setUserAttributes(LeanplumAttributes);
+//        products.clear();
+
+    }
+
+    public void trackMPevent(View view){
+        // Tracking a mParticle Custom Event passing parameters
+        eventInfo.put("spice", "hot");
+        eventInfo.put("menu", "weekdays");
+        eventInfo.put("numberParam", "123456");
+        MPEvent event = new MPEvent.Builder("Food Order", MParticle.EventType.Transaction)
+                .duration(100)
+                .info(eventInfo)
+                .build();
+
+        MParticle.getInstance().logEvent(event);
+
+
+        // tracking purchase event with mParticle
+        MParticle mp = MParticle.getInstance();
+        CommerceEvent commercEvent = new CommerceEvent.Builder(Product.PURCHASE, product)
+                .transactionAttributes(attributes)
+                .build();
+        mp.logEvent(commercEvent);
+    }
+
+    // Event parameters for mParticle
     Map<String, String> eventInfo = new HashMap<String, String>(2);
+
+    // Event paramters for Leanplum
     Map<String, Object> params = new HashMap<String, Object>();
 
 
@@ -48,6 +120,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
         Leanplum.addStartResponseHandler(new StartCallback() {
             @Override
             public void onResponse(boolean b) {
@@ -59,25 +133,9 @@ public class MainActivity extends AppCompatActivity {
                 Leanplum.track("Leanplum started", params);
 
                 // Tracking events using MParticle - you should also see those events tracked in Leanplum
-                MParticle.getInstance().logEvent("Mparticle tracked event - other", MParticle.EventType.Other);
-                MParticle.getInstance().logEvent("Mparticle tracked event - location", MParticle.EventType.Location);
-
-                // Tracking a Custom Event passing parameters
-                eventInfo.put("spice", "hot");
-                eventInfo.put("menu", "weekdays");
-                MPEvent event = new MPEvent.Builder("Food Order", MParticle.EventType.Transaction)
-                        .duration(100)
-                        .info(eventInfo)
-                        .build();
-
-                MParticle.getInstance().logEvent(event);
-
-
+//                MParticle.getInstance().logEvent("Mparticle tracked event - other", MParticle.EventType.Other);
+//                MParticle.getInstance().logEvent("Mparticle tracked event - location", MParticle.EventType.Location);
             }
         });
-
     }
 }
-
-
-
