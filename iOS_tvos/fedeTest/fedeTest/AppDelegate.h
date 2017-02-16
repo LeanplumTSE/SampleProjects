724e328a8f55ea483fa379d73119bb13ae3ef27b
federico1525 <f.casali@gmail.com>
2017-02-16 12:49:49 -0800

    iOS TV sample

---
+++ iOS_tvos/fedeTest/fedeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fedeTest
+//
+//  Created by Federico Casali on 1/31/17.
+//  Copyright © 2017 Leanplum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
