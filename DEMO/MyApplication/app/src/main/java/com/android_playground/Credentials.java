25f3f042da40c7fc7429b5d3000da5c78f51cf76
federico <f.casali@gmail.com>
2017-03-16 12:18:55 -0700

    Update Credentials.java

--- DEMO/MyApplication/app/src/main/java/com/android_playground/Credentials.java
+++ DEMO/MyApplication/app/src/main/java/com/android_playground/Credentials.java
@@ -7,10 +7,10 @@ package com.android_playground;
 public class Credentials {
 
     // AndroidApp - ID: 6258230176514048
-    public static String AppID = "app_CWa0CcxPpZD2QaR4IT7gQihLUyCJh6FF4I2lLBQgHok";
-    public static String DevKey = "dev_v4n2fDHDzAEV79DygT79zyc5zAsDquumlVFfcLMAjAY";
-    public static String ProdKey = "prod_oiRyVYqodd6d0tobY7qFeXidZJamhEUGQDfvASf6614";
+    public static String AppID = APPID;
+    public static String DevKey = DEVKEY;
+    public static String ProdKey = PRODKEY;
 
     // API Key AIzaSyA7n40b_4AuJNXfFZgxj2Cl65Bfjur5UuQ
-    public static String SenderID = "709867216442";
+    public static String SenderID = YOUR_SENDER_ID;
 }
